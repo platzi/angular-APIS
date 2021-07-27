88edbfcbc04c0a34d4aacb5d0bfe2c319a3b7934
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-27 11:30:30 -0400

    :construction: work

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -38,7 +38,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getProductsByPage(10, 0)
+    this.productsService.getAllProducts(10, 0)
     .subscribe(data => {
       this.products = data;
       this.offset += this.limit;
@@ -100,7 +100,7 @@ export class ProductsComponent implements OnInit {
   }
 
   loadMore() {
-    this.productsService.getProductsByPage(this.limit, this.offset)
+    this.productsService.getAllProducts(this.limit, this.offset)
     .subscribe(data => {
       this.products = this.products.concat(data);
       this.offset += this.limit;
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
 
@@ -20,7 +21,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
