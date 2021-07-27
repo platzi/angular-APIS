cdb925f7780efc740daf54abda91bc81c3009cf6
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-27 12:12:41 -0400

    :construction: work

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,12 +4,14 @@ import { retry } from 'rxjs/operators';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
 
+import { environment } from './../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = '/api/products';
+  private apiUrl = `${environment.API_URL}/api/products`;
 
   constructor(
     private http: HttpClient
