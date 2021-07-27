1b02b6ddd27a5b1fa7b33966280891bf0d30399a
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-27 10:12:26 -0400

    :construction: work

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -86,4 +86,14 @@ export class ProductsComponent implements OnInit {
     });
   }
 
+  deleteProduct() {
+    const id = this.productChosen.id;
+    this.productsService.delete(id)
+    .subscribe(() => {
+      const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
+      this.products.splice(productIndex, 1);
+      this.showProductDetail = false;
+    });
+  }
+
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -29,4 +29,8 @@ export class ProductsService {
   update(id: string, dto: UpdateProductDTO) {
     return this.http.put<Product>(`${this.apiUrl}/${id}`, dto);
   }
+
+  delete(id: string) {
+    return this.http.delete<boolean>(`${this.apiUrl}/${id}`);
+  }
 }
