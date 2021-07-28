282bc826f6ab7d841f948f7f3361710c81ab2832
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-28 10:58:58 -0400

    :construction_worker: work

--- src/app/interceptors/time.interceptor.ts
+++ src/app/interceptors/time.interceptor.ts
@@ -3,10 +3,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpContext,
+  HttpContextToken,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+
+const CHECK_TIME = new HttpContextToken<boolean>(() => false);
+
+export function checkTime(){
+  return new HttpContext().set(CHECK_TIME, true)
+}
 
 @Injectable()
 export class TimeInterceptor implements HttpInterceptor {
@@ -14,14 +22,17 @@ export class TimeInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const start = performance.now();
-    return next
-    .handle(request)
-    .pipe(
-      tap(() => {
-        const time = (performance.now() - start) + 'ms';
-        console.log(request.url, time);
-      })
-    );
+    if (request.context.get(CHECK_TIME)) {
+      const start = performance.now();
+      return next
+      .handle(request)
+      .pipe(
+        tap(() => {
+          const time = (performance.now() - start) + 'ms';
+          console.log(request.url, time);
+        })
+      );
+    }
+    return next.handle(request);
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,7 +4,7 @@ import { retry, catchError, map } from 'rxjs/operators';
 import { throwError, zip } from 'rxjs';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
-
+import { checkTime } from './../interceptors/time.interceptor';
 import { environment } from './../../environments/environment';
 
 @Injectable({
@@ -24,7 +24,7 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params })
+    return this.http.get<Product[]>(this.apiUrl, { params, context: checkTime() })
     .pipe(
       retry(3),
       map(products => products.map(item => {
